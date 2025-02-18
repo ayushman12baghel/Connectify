a06ce3da909e045ddc989323840e72c1b6537eee
ayushman12baghel <ayushman12baghel@gmail.com>
2025-02-18 23:35:07 +0530

    final commit

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,36 +1,53 @@
-import React from "react";
-import "../App.css";
-import { Link } from "react-router-dom";
-
+import React from 'react'
+import "../App.css"
+import { Link, useNavigate } from 'react-router-dom'
 export default function LandingPage() {
-  return (
-    <div className="landingPageContainer">
-      <nav>
-        <div className="navHeader">
-          <h2>Connectify</h2>
-        </div>
-        <div className="navlist">
-          <p>Join as Guest</p>
-          <p>Register</p>
-          <div role="button">Login</div>
-        </div>
-      </nav>
 
-      <div className="landingMainContainer">
-        <div>
-          <h1>
-            <span style={{ color: "#ff9839" }}>Connect</span> with your loved
-            ones's
-          </h1>
-          <p>Cover a distance by Connectify</p>
-          <div role="button">
-            <Link to={"/auth"}>Get Started</Link>
-          </div>
+
+    const router = useNavigate();
+
+    return (
+        <div className='landingPageContainer'>
+            <nav>
+                <div className='navHeader'>
+                    <h2>Apna Video Call</h2>
+                </div>
+                <div className='navlist'>
+                    <p onClick={() => {
+                        router("/aljk23")
+                    }}>Join as Guest</p>
+                    <p onClick={() => {
+                        router("/auth")
+
+                    }}>Register</p>
+                    <div onClick={() => {
+                        router("/auth")
+
+                    }} role='button'>
+                        <p>Login</p>
+                    </div>
+                </div>
+            </nav>
+
+
+            <div className="landingMainContainer">
+                <div>
+                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+
+                    <p>Cover a distance by Apna Video Call</p>
+                    <div role='button'>
+                        <Link to={"/auth"}>Get Started</Link>
+                    </div>
+                </div>
+                <div>
+
+                    <img src="/mobile.png" alt="" />
+
+                </div>
+            </div>
+
+
+
         </div>
-        <div>
-          <img src="/mobile.png" alt="" />
-        </div>
-      </div>
-    </div>
-  );
+    )
 }
