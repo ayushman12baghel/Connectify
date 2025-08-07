526eebc7043872ca94b3f83c6b881a38c8107875
ayushman12baghel <ayushman12baghel@gmail.com>
2025-08-07 15:22:32 +0530

    Fix API paths and CORS for production

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,6 @@
 let IS_PROD = process.env.NODE_ENV === "production";
 const server = IS_PROD ?
-    "/api/v1" :
-    "http://localhost:8000/api/v1"
+    "/api" :
+    "http://localhost:8000"
 
 export default server;
